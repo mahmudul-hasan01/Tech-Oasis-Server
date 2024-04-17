75f8758ccf2113df7730b728823d6ac21996379e
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-04-17 15:41:31 +0600

    get post and delete shoping info

--- PathHandler/AddItemHandlers.js
+++ PathHandler/AddItemHandlers.js
@@ -2,17 +2,12 @@ const express = require('express')
 const router = express.Router()
 const addItemSchema = require('../Schema/AddItemSchema')
 
-// get all data
-router.get('/', async (req, res) => {
-    const users = await userModel.find({})
-    res.send(users)
-})
 
-// get a user
-router.get('/:id', async (req, res) => {
-    const user = await userModel.findById(req.params.id)
-    if (!user) return res.send(404).json({ error: 'user not found' })
-    res.send(user)
+// get a data
+router.get('/:email', async (req, res) => {
+    const data = await addItemSchema.find({ email: req.params.email })
+    if (!data) return res.send(404).json({ error: 'data not found' })
+    res.send(data)
 })
 
 // post data
@@ -22,17 +17,11 @@ router.post('/', async (req, res) => {
     res.status(201).send(result)
 })
 
-
-//   // put data
-//   router.patch('/:id', async (req, res) => {
-//     const updatadata = await addItemSchema.findByIdAndUpdate(req.params.id)
-//     res.send(updatadata)
-//   })
-
-//   // delete data
-//   router.delete('/:id', async (req, res) => {
-//       const deletedata = await addItemSchema.findByIdAndDelete(req.params.id)
-//       res.send(deletedata)
-//   })
+// delete data
+router.delete('/:id', async (req, res) => {
+    console.log(req.params.id);
+    const deletedata = await addItemSchema.findByIdAndDelete(req.params.id)
+    res.send(deletedata)
+})
 
 module.exports = router
