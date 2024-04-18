460ba0ae396ba6e3efea312376dba305a4cf5788
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-04-18 15:34:07 +0600

    get payment history

--- PathHandler/paymentHandlers.js
+++ PathHandler/paymentHandlers.js
@@ -2,12 +2,18 @@ const express = require('express')
 const router = express.Router()
 const paymentModel = require('../Schema/paymentSchema')
 
+// get a data
+router.get('/:email', async (req, res) => {
+  const data = await paymentModel.find({ email: req.params.email })
+  if (!data) return res.send(404).json({ error: 'data not found' })
+  res.send(data)
+})
 
-  // post data
-  router.post('/', async (req, res) => {
-      const data = req.body
-      const result = await paymentModel.create(data)
-      res.status(201).send(result)
-  })
+// post data
+router.post('/', async (req, res) => {
+  const data = req.body
+  const result = await paymentModel.create(data)
+  res.status(201).send(result)
+})
 
 module.exports = router
